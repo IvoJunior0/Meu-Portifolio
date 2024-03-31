756bec555ea9c71eb80605a1269bd53fdad9d323
IvoJunior0 <shymanjr2019@gmail.com>
2024-03-31 18:52:03 -0300

    menu da barra de navegação no celular adicionada

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-let menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px";
+const menuNavBar = document.getElementById("nav-menu");
+const menuClose = document.getElementById("menu-close");
 
-function toggleMenu(){
-    let menuListNewHeight = menuList.style.maxHeight === "0px" ? "300px" : "0px";
-    menuList.style.maxHeight = menuListNewHeight;
+function closeMenu(){
+    let menuNavBarWidth = menuNavBar.offsetWidth;
+    let menuNavBarLeft = menuNavBar.style.left;
+    menuNavBar.style.left = `${parseInt(menuNavBarLeft,10) - menuNavBarWidth}px`;
+    console.log(menuNavBarWidth);
+    console.log(menuNavBarLeft);
+    console.log("foi");
 }
 
-function goToForm() {
-    window.location.href = 'pages/formulario.html';
+function openMenu(){
+    menuNavBar.style.left = `0px`;
 }
