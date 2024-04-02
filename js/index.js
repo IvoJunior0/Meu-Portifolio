19a8e46b2d8319d984dd8a73799f4b719e411648
IvoJunior0 <shymanjr2019@gmail.com>
2024-04-01 23:10:43 -0300

    ajustes: caixas de qualidades

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 const menuNavBar = document.getElementById("nav-menu");
 const menuLinks = document.querySelectorAll(".links");
 
+menuLinks.forEach((element) => {
+    element.addEventListener("click", () => {
+        closeMenu();
+    });
+});
+
+function goToForm() {
+    window.location.href = 'pages/formulario.html';
+}
+
 function closeMenu(){
     let menuNavBarWidth = menuNavBar.offsetWidth;
     menuNavBar.style.left = `${0 - menuNavBarWidth}px`;
@@ -9,9 +19,3 @@ function closeMenu(){
 function openMenu(){
     menuNavBar.style.left = `0px`;
 }
-
-menuLinks.forEach((element) => {
-    element.addEventListener("click", () => {
-        closeMenu();
-    });
-});
