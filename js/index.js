26ab9977227e1fcf2ff67ae38fdb37c0f05f85e8
IvoJunior0 <shymanjr2019@gmail.com>
2024-04-01 21:40:13 -0300

    página de respota do formulário adicionada

--- js/index.js
+++ js/index.js
@@ -3,11 +3,7 @@ const menuClose = document.getElementById("menu-close");
 
 function closeMenu(){
     let menuNavBarWidth = menuNavBar.offsetWidth;
-    let menuNavBarLeft = menuNavBar.style.left;
-    menuNavBar.style.left = `${parseInt(menuNavBarLeft,10) - menuNavBarWidth}px`;
-    console.log(menuNavBarWidth);
-    console.log(menuNavBarLeft);
-    console.log("foi");
+    menuNavBar.style.left = `${0 - menuNavBarWidth}px`;
 }
 
 function openMenu(){
