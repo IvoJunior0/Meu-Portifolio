cb05215cc00004e2c647f699ae5ae48deb1e7de7
IvoJunior0 <shymanjr2019@gmail.com>
2024-04-13 23:15:30 -0300

    sobre mim tecnologias adicionado

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
-const menuNavBar = document.getElementById("nav-menu");
-const menuLinks = document.querySelectorAll(".links");
+// Redirecionando para outras páginas
+function goToForm() {
+    window.location.href = 'pages/formulario.html';
+}
 
-menuLinks.forEach((element) => {
+function goToLDG() {
+    window.location.href = 'https://open.spotify.com/intl-pt/album/1lcaJxO3cm4fkXjbBDJoaa';
+}
+
+// Menu responsivo
+const menuNavBar = document.getElementById("nav-menu");
+const menuLinks = document.querySelectorAll(".links"); // Opções do menu
+
+menuLinks.forEach((element) => { // Fechar o menu quando uma opção for clicada
     element.addEventListener("click", () => {
         closeMenu();
     });
 });
 
-function goToForm() {
-    window.location.href = 'pages/formulario.html';
-}
-
+// Funções de interação com o menu
 function closeMenu(){
     let menuNavBarWidth = menuNavBar.offsetWidth;
     menuNavBar.style.left = `${0 - menuNavBarWidth}px`;
@@ -19,3 +26,20 @@ function closeMenu(){
 function openMenu(){
     menuNavBar.style.left = `0px`;
 }
+
+// Easter-egg do mario
+const mario = document.querySelector('.mario');
+let marioJumping = false;
+
+mario.addEventListener("click", () => {
+    if (!marioJumping) {
+        let marioSfx = new Audio('../audio/mario-jump.mp3');
+        marioSfx.play();
+    }
+    marioJumping = true;
+    mario.classList.add("mario-jumping");
+    mario.addEventListener("animationend", () => {
+        mario.classList.remove("mario-jumping");
+        marioJumping = false;
+    }, false);
+});
